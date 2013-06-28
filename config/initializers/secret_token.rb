fa13702642eb222f6cc6ca34ac75604d06a7ff3e
Chris Mailloux <chrismayu@gmail.com>
2013-06-28 12:14:18 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReefSense::Application.config.secret_token = '574de74921e228893647960b2932cac738185b07166caa54d178e127e9caa9f9dd9a97b4597ee42d7f451bebcaeed8c01dcc82e87e2284bae2c5970b903e8f03'
