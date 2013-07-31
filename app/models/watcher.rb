d3f4285a75b3e80dc779a5beb925c469f04ae4c7
Chris Mailloux <chrismayu@gmail.com>
2013-07-31 18:47:21 -0400

    Changed order of watchers

--- app/models/watcher.rb
+++ app/models/watcher.rb
@@ -1,6 +1,7 @@
 class Watcher < ActiveRecord::Base
   attr_accessible :ambeint_temp, :ato_pump,:waterlevelokay, :startingup, :chiller, :heater, :main_light, :main_pump, :params, :ph_level, :powerhead, :reef_tank_arduino_id, :reef_tank_id, :refuge_light, :skimmer, :temp, :updated_reason
 
+ default_scope order: 'watchers.created_at DESC'
 
  
        belongs_to :reef_tank, :primary_key => :reef_tank_arduino_id , :foreign_key => :reef_tank_arduino_id
